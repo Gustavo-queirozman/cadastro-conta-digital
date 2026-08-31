4378c89f88ef643d9f48f3aaf84779b8b7debc12
Gustavo-queirozman <gustavoqueirozmit@gmail.com>
2026-08-31 17:03:20 -0300

    Create routers

--- config/routes.php
+++ config/routes.php
@@ -15,6 +15,34 @@ use Hyperf\HttpServer\Router\Router;
 Router::addGroup('/v1', function(){
     Router::addGroup('/users', function(){
         Router::post('/draft', 'App\Controller\UserController@draft');
+        Router::post('/verify-phone', 'App\Controller\UserController@verifyPhone');
+        Router::post('/verify-email', 'App\Controller\UserController@verifyEmail');
+        Router::post('/{id}/address', 'App\Controller\UserController@registerAddress');
+        Router::post('/{id}/documents', 'App\Controller\UserController@registerDocument');
+        Router::post('/{id}/biometrics', 'App\Controller\UserController@biometricFace');
+    });
+
+    Router::addGroup('/accounts', function(){
+        Router::post('/', 'App\Controller\AccountController@registerAccount');
+        Router::get('/status', 'App\Controller\AccountController@getAccountStatus');
+        Router::get('/me', 'App\Controller\AccountController@getAccountInfo');
+        Router::patch('/status', 'App\Controller\AccountController@updateAccountStatus');
+    });
+
+    Router::addGroup('/auth', function(){
+        Router::post('/password/setup', 'App\Controller\AuthController@registerSetupPassword');
+        Router::post('/pin/setup', 'App\Controller\AuthController@registerSetupPin');
+        Router::post('/login', 'App\Controller\AuthController@login'); 
+    });
+});
+
+Router::addGroup('/internal/v1', function (){
+    Router::addGroup('/users', function(){
+        Router::get('/{id}', 'App\Controller\UserController@getUserInfo');
+    });
+
+    Router::addGroup('/accounts', function(){
+        Router::get('/{id}/compliance', 'App\Controller\AccountController@getAccountCompliance');
     });
 });
 
